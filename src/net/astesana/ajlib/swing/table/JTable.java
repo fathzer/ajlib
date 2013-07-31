ae2e8b2ec647075ef44656cc88217c1163a7491a
fathzer <fathzer@a7afff35-c381-47ed-af84-387d70dac988>
2013-07-31 13:23:23 +0000

    SICG:1 JMA commit

--- src/net/astesana/ajlib/swing/table/JTable.java
+++ src/net/astesana/ajlib/swing/table/JTable.java
@@ -1,8 +1,9 @@
 package net.astesana.ajlib.swing.table;
 
+import javax.swing.ListSelectionModel;
 import javax.swing.table.TableModel;
 
-/** A JTable that fixes the following bugs in the JTable.
+/** A JTable that fixes the following bugs in the JTable and adds some functionalities.
  * <br>Bugs fixed:<ul>
  * <li>Unlike the original swing JTable class, the row height is set accordingly to the font (see setRowHeigth() method).
  * <br>The method setRowHeight is called in the constructor. You should call it again when changing the font.</li>
@@ -27,5 +28,23 @@ public class JTable extends javax.swing.JTable {
 	 */
 	public void setRowHeight() {
 		setRowHeight(getFont().getSize()*4/3);
+	}
+	
+	/** Sets the selection.
+	 * @param selectedIndexes The indexes to select.<br>Please note that specifying non contiguous selection on a table that
+	 * uses SINGLE_SELECTION mode may have unpredictable result.
+	 * @throws IllegalArgumentException if an index is out of bounds.
+	 */
+	public void setSelectedIndexes(int[] selectedIndexes) {
+		for (int i = 0; i < selectedIndexes.length; i++) {
+			if ((selectedIndexes[i]<0) || (selectedIndexes[i]>=getRowCount())) throw new IllegalArgumentException();
+		}
+		ListSelectionModel selectionModel = getSelectionModel();
+		selectionModel.setValueIsAdjusting(true);
+		selectionModel.clearSelection();
+		for (int i = 0; i < selectedIndexes.length; i++) {
+			selectionModel.addSelectionInterval(selectedIndexes[i], selectedIndexes[i]);
+		}
+		selectionModel.setValueIsAdjusting(false);
 	}
 }
